0f8db1f2fca352786dbbf8c143419d77bc0d9fc4
AyobamiLad <moriahberry2222@gmail.com>
2024-06-12 18:00:09 +0100

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,8 +15,8 @@ const upload = multer({ dest: 'uploads/' });
 const app = express();
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
   origin: 'http://localhost:3000', // or your frontend URL
   credentials: true,
