8d845da96923b7e608dd8dd4d58e9b2cbb813d30
Adebayour66265 <adebayour66265@gmail.com>
2024-08-09 18:56:15 +0100

    new commit message

--- frontend/src/context/Auth.tsx
+++ frontend/src/context/Auth.tsx
@@ -7,8 +7,8 @@ type ContainerProps = {
 };
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://b2b-saas-lead-mangement-main.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://b2b-saas-lead-mangement-main.onrender.com/api",
   // timeout: 5000,
   withCredentials: true,
   headers: {
